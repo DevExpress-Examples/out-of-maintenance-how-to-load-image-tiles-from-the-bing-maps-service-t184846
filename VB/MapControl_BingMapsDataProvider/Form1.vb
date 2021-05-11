c1d264c2d34a7ee5319ec9e76388bd3c4a5f83cc
DevExpressExampleBot <fakeEmail>
2021-05-11 13:55:58 +0300

    Source auto update [skip ci]

--- VB/MapControl_BingMapsDataProvider/Form1.vb
+++ VB/MapControl_BingMapsDataProvider/Form1.vb
@@ -2,16 +2,19 @@
 Imports DevExpress.XtraMap
 
 Namespace MapControl_BingMapsDataProvider
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-'            #Region "#BingMapsDataProvider"
-            mapControl1.Layers.Add(New ImageLayer() With { _
-                .DataProvider = New BingMapDataProvider() With {.BingKey = "YOUR BING KEY"} _
-            })
-'            #End Region ' #BingMapsDataProvider
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+'			#Region "#BingMapsDataProvider"
+			mapControl1.Layers.Add(New ImageLayer() With {
+				.DataProvider = New BingMapDataProvider() With {
+					.BingKey = "YOUR BING KEY",
+					.Kind = BingMapKind.Road
+				}
+			})
+'			#End Region ' #BingMapsDataProvider
+		End Sub
+	End Class
 End Namespace
